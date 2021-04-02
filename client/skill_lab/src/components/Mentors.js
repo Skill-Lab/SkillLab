6de91f758f3c515ffe9235799a22c6ac43da8789
Alexis Huerta <alehuer90@gmail.com>
2021-04-02 11:31:48 -0700

    merge

--- client/skill_lab/src/components/Mentors.js
+++ client/skill_lab/src/components/Mentors.js
@@ -6,13 +6,8 @@ import AccordionDetails from "@material-ui/core/AccordionDetails";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
-<<<<<<< HEAD
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
-import { Redirect } from "react-router";
-=======
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { Redirect, useHistory } from "react-router";
->>>>>>> a789ba1a76a2281a9333df9ee0559a1c163526a2
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,15 +19,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-<<<<<<< HEAD
-//Direct to group subspace page
-const goToSubspace = (subspaceName) => {
-  <Redirect to={`/subspace/`} />;
-  console.log(subspaceName);
-};
-=======
-
->>>>>>> a789ba1a76a2281a9333df9ee0559a1c163526a2
 export default function Mentors({ name, list }) {
   const history = useHistory();
   const classes = useStyles();
@@ -54,21 +40,9 @@ export default function Mentors({ name, list }) {
         >
           <Typography className={classes.heading}>{name}</Typography>
         </AccordionSummary>
-<<<<<<< HEAD
-        {list.map((text) => (
-          <AccordionDetails key={text}>
-            <ListItem
-              onClick={() => goToSubspace(text)}
-              value={text}
-              button
-              key={text}
-            >
-=======
         {list.map((mentor) => (
-          
           <AccordionDetails key={mentor.id}>
-            <ListItem key={mentor.id} value={mentor.name} button >
->>>>>>> a789ba1a76a2281a9333df9ee0559a1c163526a2
+            <ListItem key={mentor.id} value={mentor.name} button>
               <ListItemIcon>
                 <AccountCircleIcon />
               </ListItemIcon>
