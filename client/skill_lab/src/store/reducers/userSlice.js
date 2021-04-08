bab97f4bda8a501d6e6945a720999a2964165a65
Nathan <abegaznathan@gmail.com>
2021-04-08 01:06:40 -0700

    Revert "Revert "Update userSlice.js""
    This reverts commit 76215a0cfef2909f66e6909cf1e6d0f9f287bafd.

--- client/skill_lab/src/store/reducers/userSlice.js
+++ client/skill_lab/src/store/reducers/userSlice.js
@@ -4,20 +4,25 @@ export const userSlice = createSlice({
   name: "user",
   initialState: {
     user: null,
+    groups: [],
   },
   reducers: {
-      login: (state,action) =>{
-          state.user = action.payload;
-      },
-      logout: (state)=>{
-          state.user= null;
-      }
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
+    addGroups: (state, action) => {
+      state.groups = action.payload;
+    },
   },
 });
 
-export const {login, logout} = userSlice.actions;
+export const { login, logout, addGroups } = userSlice.actions;
 
 //Selectors
 export const selectUser = (state) => state.user.user;
+export const selectGroups = (state) => state.user.groups;
 
 export default userSlice.reducer;
