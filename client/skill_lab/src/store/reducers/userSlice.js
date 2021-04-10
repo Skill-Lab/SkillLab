9e8b10ee5953801bc2563d8d2fbb14e0c1a767b9
Nathan <abegaznathan@gmail.com>
2021-04-09 18:43:56 -0700

    Update userSlice.js
    Addded dispatches to redux:
     - AddGroup
     - leaveGroup

--- client/skill_lab/src/store/reducers/userSlice.js
+++ client/skill_lab/src/store/reducers/userSlice.js
@@ -12,17 +12,24 @@ export const userSlice = createSlice({
     },
     logout: (state) => {
       state.user = null;
+      state.groups = [];
     },
-    addGroups: (state, action) => {
+    storeGroups: (state, action) => {
       state.groups = action.payload;
     },
+    addGroup: (state, action) => {
+      state.groups.groups = [...state.groups.groups, action.payload];  
+    },
+    leaveGroup: (state,action) => {
+      state.groups.groups = state.groups.groups.filter(group => group.id !== action.payload)
+    }
   },
 });
 
-export const { login, logout, addGroups } = userSlice.actions;
+export const { login, logout, storeGroups, addGroup, leaveGroup } = userSlice.actions;
 
 //Selectors
 export const selectUser = (state) => state.user.user;
-export const selectGroups = (state) => state.user.groups;
+export const selectGroups = (state) => state.user.groups.groups;
 
 export default userSlice.reducer;
