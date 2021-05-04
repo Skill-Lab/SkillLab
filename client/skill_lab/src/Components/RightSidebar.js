eaaf9c8d9272cf5c00d51d7acf72e063380297ce
Nathan <abegaznathan@gmail.com>
2021-05-03 21:45:39 -0700

    Added Mentor designation in subspace

--- client/skill_lab/src/Components/RightSidebar.js
+++ client/skill_lab/src/Components/RightSidebar.js
@@ -1,6 +1,7 @@
 import { Divider, Drawer, makeStyles, Toolbar } from "@material-ui/core";
 import React from "react";
 import ProfileList from "./ProfileList";
+import Switch from "@material-ui/core/Switch";
 
 const drawerWidth = 300;
 
@@ -23,7 +24,13 @@ const useStyles = makeStyles((theme) => ({
 
 export default function RightSidebar({ description, members }) {
   const classes = useStyles();
-
+  const [state, setState] = React.useState({
+    checkedB: false,
+  });
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+    console.log(state.checkedB);
+  };
   return (
     <div>
       <Drawer
@@ -38,7 +45,20 @@ export default function RightSidebar({ description, members }) {
         <div className={classes.drawerContainer}>
           <div className={classes.description}>{description}</div>
           <Divider />
+          <div className={classes.description}>
+            <h4>
+              Mentor this Subspace
+              <Switch
+                checked={state.checkedB}
+                onChange={handleChange}
+                color="primary"
+                name="checkedB"
+                inputProps={{ "aria-label": "primary checkbox" }}
+              />
+            </h4>
+          </div>
           <ProfileList name="Members" list={members} />
+          <ProfileList name="Mentors" list={members} />
         </div>
       </Drawer>
     </div>
